3a3e3bfe30b3b09ded8a239db08ffdf33e0ebebc
KemboiHub <lexinclassics@gmail.com>
2025-10-28 15:01:08 +0300

    new features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,87 @@ const Header: React.FC<HeaderProps> = ({ activeSection, navigateToSection, setAc
     { id: 'blog', label: 'BLOG' },
     { id: 'contact', label: 'CONTACT US' }
   ];
+const PaymentForm = () => {
+  const [phone, setPhone] = useState('');
+  const [accountNumber, setAccountNumber] = useState('');
+  const [amount, setAmount] = useState('');
+  const [message, setMessage] = useState('');
 
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleAccountNumberChange = (e) => {
+    setAccountNumber(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/api/stkpush", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ phone: phone, accountNumber: accountNumber, amount: amount }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage('Payment successful!');
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage('Payment failed!');
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={styles.form}>
+      <h1>Mpesa Payment Form</h1>
+      {message && <p style={{ color: message.includes('failed') ? 'red' : 'green' }}>{message}</p>}
+      <div style={styles.formGroup}>
+        <label htmlFor="phone" style={styles.label}>Phone Number</label>
+        <input
+          type="number"
+          id="phone"
+          value={phone}
+          onChange={handlePhoneChange}
+          style={styles.input}
+          required
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label htmlFor="accountNumber" style={styles.label}>Account Number</label>
+        <input
+          type="text"
+          id="accountNumber"
+          value={accountNumber}
+          onChange={handleAccountNumberChange}
+          style={styles.input}
+          required
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label htmlFor="amount" style={styles.label}>Amount</label>
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={handleAmountChange}
+          style={styles.input}
+          required
+        />
+      </div>
+      <button type="submit" style={styles.button}>Pay Now</button>
+
+
+    </form>
+  );
+};
   // handle checkout: initiates STK push via backend (use phone from user profile)
   const handleCheckout = async () => {
     if (!isLoggedIn) {
