bc9b3d757cb7dc5e0b035c3df40640cc04da354e
KemboiHub <lexinclassics@gmail.com>
2025-11-01 16:02:42 +0300

    new features

--- Front-end/src/components/detete.tsx
+++ Front-end/src/components/detete.tsx
@@ -79,4 +79,6 @@ const BlogSection: React.FC<BlogSectionProps> = ({ goBack }) => {
     { name: "All Posts", count: 24, active: true },
 
     { name: "Care Guide", count: 8 },
+    { name: "Health", count: 6 },
+    { name: "Nutrition", count: 5 },    
   
